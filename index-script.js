bb1514da2740bfb9ba63da78ac7f9cedc7d9e887
Jana Sandeyouni <Jnoo66@icloud.com>
2026-06-09 16:31:13 +0300

    Add orders list

--- index-script.js
+++ index-script.js
@@ -8,3 +8,16 @@ function getLocation() {
   });
 }
  
+// Add order to the list on the page
+function addOrder() {
+  const name = document.getElementById("name").value;
+  const cake = document.getElementById("cake").value;
+ 
+  if (name === "") { alert("Please enter your name first!"); return; }
+ 
+  const li = document.createElement("li");
+  li.textContent = name + " ordered " + cake;
+  document.getElementById("ordersList").appendChild(li);
+ 
+  document.getElementById("name").value = ""; // clear the name field
+}
